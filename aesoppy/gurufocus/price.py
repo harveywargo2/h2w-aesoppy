fb5b951b98de5d6c7f5caa8ee28fa580ab0c3bf1
Harvey Wargo 2 <16654472+harveywargo2@users.noreply.github.com>
2024-09-30 16:08:45 -0400

    Update price.py

--- aesoppy/gurufocus/price.py
+++ aesoppy/gurufocus/price.py
@@ -3,16 +3,6 @@ import pandas as pd
 
 
 class PriceHistory:
-    """
-    Description:
-        GuruFocus api call to historical price data
-    Args:
-        token (string): GuruFocus API Token
-        ticker (string): Stock Ticker
-
-    Returns:
-        api_data (list): raw api output from api call
-    """
 
     def __init__(self, **kwargs):
         self.token = kwargs.get('token', 'error')
@@ -23,16 +13,7 @@ class PriceHistory:
         return requests.get(f'https://api.gurufocus.com/public/user/{str(self.token)}/stock/{str(self.ticker)}/price').json()
 
     def price_df(self, **kwargs):
-        """
-        Description:
-            Transformed DataFrame Object
-        Args:
-            index_date (boolean):
-                True (default): Date set to Index
-                False: integer index
-        Returns:
-            Pandas Dataframe of Indexed Price History
-        """
+
         self.index_exdate = kwargs.get('index_date', True)
         price_list = self.price_data
         price_df = pd.DataFrame(price_list, columns=['Date', 'SharePrice'])
