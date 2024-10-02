eeff37d6de5cb27d3c6fc6125fef3bd077ba105e
Harvey Wargo 2 <16654472+harveywargo2@users.noreply.github.com>
2024-10-02 12:29:16 -0400

    Update analysis_wrangler.py

--- aesoppy/gurufocus/analysis_wrangler.py
+++ aesoppy/gurufocus/analysis_wrangler.py
@@ -71,11 +71,14 @@ class AnnualFinAnalysis:
         self.analysis_raw_data = gf.Financials(token=self.token, ticker=self.ticker).annual_data
         self.analysis_raw_data_reit = gf.Financials(token=self.token, ticker=self.ticker).reit
         self.analysis_norm_data = self._analysis_data_normalization()
-        self.analysis_fy_cash = ''
-        self.analysis_fy_debt = ''
-        self.analysis_fy_growth = ''
-        self.analysis_fy_owner = ''
-        self.analysis_fy_cost = ''
+        self.analysis_fy_cash_flow = self._analysis_fy_cash_flow()
+        self.analysis_fy_cost_flow = self._analysis_fy_cost_flow()
+        self.analysis_fy_debt = self._analysis_fy_debt()
+        self.analysis_fy_owner = self._analysis_fy_owner()
+        self.analysis_fy_growth = self._analysis_fy_growth()
+        self.analysis_per_share = self._analysis_fy_pershare()
+
+
 
 
     def _analysis_data_normalization(self):
@@ -140,7 +143,7 @@ class AnnualFinAnalysis:
             fy_df2 # Do Nothing
 
         if any(fy_df1.columns == 'balance_sheet.Cash and Cash Equivalents'):
-            fy_df2['CashAndEquivalents'] = fy_df1['balance_sheet.Cash and Cash Equivalents'].astype(float)
+            fy_df2['CashEquivalents'] = fy_df1['balance_sheet.Cash and Cash Equivalents'].astype(float)
         else:
             fy_df2 # Do Nothing
 
@@ -191,6 +194,11 @@ class AnnualFinAnalysis:
 
         if any(fy_df1.columns == 'cashflow_statement.Capital Expenditure'):
             fy_df2['CAPEX'] = fy_df1['cashflow_statement.Capital Expenditure'].astype(float)
+        else:
+            fy_df2 # Do Nothing
+
+        if any(fy_df1.columns == 'cashflow_statement.Free Cash Flow'):
+            fy_df2['FreeCashFlow'] = fy_df1['cashflow_statement.Free Cash Flow'].astype(float)
         else:
             fy_df2 # Do Nothing
 
@@ -265,6 +273,64 @@ class AnnualFinAnalysis:
             fy_df2 # Do Nothing
 
         return fy_df2
+
+    def _analysis_fy_cash_flow(self):
+        cash_df1 = self.analysis_norm_data
+        cash_df2 = pd.DataFrame(index=cash_df1.index)
+
+        cash_df2 = cash_df1[['Revenue', 'GrossProfit', 'OperatingIncome', 'NetIncome', 'CashFromOperations',
+                             'FreeCashFlow']]
+
+        return cash_df2
+
+    def _analysis_fy_cost_flow(self):
+        cost_df1 = self.analysis_norm_data
+        cost_df2 = pd.DataFrame(index=cost_df1.index)
+
+        cost_df2 = cost_df1[['Revenue', 'COGS', 'SGA', 'RND', 'OperatingExpense',
+                             'TaxPaid', 'CAPEX']]
+
+        return cost_df2
+
+    def _analysis_fy_debt(self):
+        debt_df1 = self.analysis_norm_data
+        debt_df2 = pd.DataFrame(index=debt_df1.index)
+
+        debt_df2 = debt_df1[['Revenue', 'CashFromOperations', 'FreeCashFlow', 'CashEquivalents', 'MarketSecurities',
+                             'TreasuryStock', 'CurrentAssets', 'LongAssets', 'CurrentLiabilities', 'LongLiabilities',
+                             'CashPaidToDebt']]
+
+        return debt_df2
+
+    def _analysis_fy_owner(self):
+        owner_df1 = self.analysis_norm_data
+        owner_df2 = pd.DataFrame(index=owner_df1.index)
+
+        owner_df2 = owner_df1[['Revenue', 'FreeCashFlow', 'CashEquivalents', 'MarketSecurities',
+                               'TreasuryStock', 'CashForDividends', 'StockIssuance', 'StockRepurchase']]
+
+        return owner_df2
+
+    def _analysis_fy_growth(self):
+        growth_df1 = self.analysis_norm_data
+        growth_df2 = pd.DataFrame(index=growth_df1.index)
+
+        growth_df2 = growth_df1[['Revenue', 'CashEquivalents', 'MarketSecurities', 'SharesOutBasic', 'SharesOutDiluted',
+                                 'CashForDividends']]
+
+        return growth_df2
+
+    def _analysis_fy_pershare(self):
+        ps_df1 = self.analysis_norm_data
+        ps_df2 = pd.DataFrame(index=ps_df1.index)
+
+        ps_df2 = ps_df1[['PerShareRevenue', 'PerShareEarnings', 'PerShareFcf', 'PerShareDividends',
+                         'StockPriceHigh', 'StockPriceLow']]
+
+        return ps_df2
+
+
+
 
 
 
