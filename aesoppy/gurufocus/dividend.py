152f194c7de46918d784ad1095ee3de01ff4d2d7
Harvey Wargo 2 <16654472+harveywargo2@users.noreply.github.com>
2024-12-30 18:19:57 -0500

    Update dividend.py

--- aesoppy/gurufocus/dividend.py
+++ aesoppy/gurufocus/dividend.py
@@ -10,7 +10,7 @@ class GuruDividendHistory:
         self.api_data = self._api_data()
         self.api_data_type = type(self.api_data)
         self.api_data_df = self._api_data_df()
-        self.api_data_df_ex_date_index = self._api_data_df_ex_date_index()
+        self.aesop_normalized = self._aesop_normalized()
 
 
     def _api_data(self):
@@ -24,17 +24,23 @@ class GuruDividendHistory:
         return div_df
 
 
-    def _api_data_df_ex_date_index(self):
+    def _aesop_normalized(self):
 
         div_list = self.api_data
         div_df = pd.DataFrame(div_list)
-        div_df['ex_date'] = pd.to_datetime(div_df['ex_date'])
-        div_df['amount'] = div_df['amount'].astype(float)
-        div_df.set_index('ex_date', inplace=True)
-        div_df.rename(columns={
+        div_df = div_df.rename(columns={
             'amount': 'dividend_amount',
             'type': 'dividend_type'
         })
+        div_df['dividend_type'] = div_df['dividend_type'].replace('Cash Div.', 'regular')
+        div_df['dividend_type'] = div_df['dividend_type'].replace('Special Div.', 'special')
+
+        div_df['ex_date'] = pd.to_datetime(div_df['ex_date'])
+        div_df['record_date'] = pd.to_datetime(div_df['record_date'])
+        #div_df['pay_date'] = pd.to_datetime(div_df['pay_date'])
+
+        div_df['dividend_amount'] = div_df['dividend_amount'].astype(float)
+
 
         return div_df
 
