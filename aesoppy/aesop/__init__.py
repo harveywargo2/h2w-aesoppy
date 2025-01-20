9ed46d9874f5a4e8cc7d0d8fef8e5897cd20fb1c
Harvey Wargo 2 <16654472+harveywargo2@users.noreply.github.com>
2025-01-20 12:48:03 -0500

    Update __init__.py

--- aesoppy/aesop/__init__.py
+++ aesoppy/aesop/__init__.py
@@ -1,5 +1,6 @@
 from aesoppy.aesop.common import *
-from aesoppy.aesop.dividend_coverage import (DividendCoverage)
+from aesoppy.aesop.dividend_coverage import DividendCoverage
 from aesoppy.aesop.dividend_growth import (DividendPerShareCy, DividendPerShareFy, DividendTotalCashFy)
-from aesoppy.aesop.dividend_yield_theory import (DividendYieldTheory)
+from aesoppy.aesop.dividend_yield_theory import DividendYieldTheory
+from aesoppy.aesop.margins import Margins
 
